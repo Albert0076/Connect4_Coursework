b1a5b5d1b072981507b1b5da0750e6ec83e6f9a1
Albert0076 <albain0809@highgateschool.org.uk>
2024-05-07 09:32:16 +0100

    Added play_self function, alpha_beta_minimax prototype and timing functions to see how the alpha_beta function compares to the original.

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,15 @@
+import time
+import random
+
+
 class Grid:
     def __init__(self):
-        self.array = [[Symbol() for j in range(3)] for i in range(3)]
+        self.array = [[Symbol() for _ in range(3)] for _ in range(3)]
 
     def add_piece(self, column, row, symbol):
+        if self.is_full():
+            raise IndexError("Grid is Full")
+
         self.array[column][row].set_symbol(symbol)
 
     def check_win(self):
@@ -39,7 +46,7 @@ class Grid:
                 else:
                     return_str += self.array[i][j].symbol
 
-            return_str += ("\n")
+            return_str += "\n"
 
         return return_str
 
@@ -81,6 +88,47 @@ def minimax(grid, depth, is_max, player_symbol, opp_symbol):
         return best
 
 
+def minimax_alpha_beta(grid, depth, is_max, player_symbol, opp_symbol, alpha=-1000, beta=1000):
+    score = evaluate(grid, player_symbol)
+
+    if score == 10:
+        return score
+
+    if score == -10:
+        return score
+
+    if grid.is_full():
+        return 0
+
+    if is_max:
+        best = -1000
+        for column in grid.array:
+            for piece in column:
+                if piece.is_empty():
+                    piece.set_symbol(player_symbol)
+                    best = max(best, minimax_alpha_beta(grid, depth + 1, False, player_symbol, opp_symbol, alpha, beta))
+                    alpha = max(best, alpha)
+                    piece.reset_symbol()
+                    if beta <= alpha:
+                        break
+
+        return best
+
+    else:
+        best = 1000
+        for column in grid.array:
+            for piece in column:
+                if piece.is_empty():
+                    piece.set_symbol(opp_symbol)
+                    best = min(best, minimax_alpha_beta(grid, depth + 1, True, player_symbol, opp_symbol, alpha, beta))
+                    beta = min(best, beta)
+                    piece.reset_symbol()
+                    if beta <= alpha:
+                        break
+
+        return best
+
+
 def find_best_move(grid, player="X", opp="O"):
     best_val = -1000
     best_move = (-1, -1)
@@ -90,6 +138,25 @@ def find_best_move(grid, player="X", opp="O"):
                 grid.array[i][j].set_symbol(player)
 
                 move_val = minimax(grid, 0, False, player, opp)
+
+                grid.array[i][j].reset_symbol()
+
+                if move_val > best_val:
+                    best_move = (i, j)
+                    best_val = move_val
+
+    return best_move
+
+
+def find_best_move_alpha_beta(grid, player="X", opp="O"):
+    best_val = -1000
+    best_move = (-1, -1)
+    for i in range(3):
+        for j in range(3):
+            if grid.array[i][j].is_empty():
+                grid.array[i][j].set_symbol(player)
+
+                move_val = minimax_alpha_beta(grid, 0, False, player, opp)
 
                 grid.array[i][j].reset_symbol()
 
@@ -126,10 +193,58 @@ class Symbol:
         return self.symbol is None
 
     def __eq__(self, other):
-        return self.symbol == other.symbol and not self.symbol is None
+        return not (not (self.symbol == other.symbol) or self.symbol is None)
+
+
+def play_self():
+    grid = Grid()
+    while not (grid.check_win()[0] or grid.is_full()):
+        p1_move = find_best_move(grid, "X", "O")
+        if not p1_move == (-1, -1):
+            grid.add_piece(p1_move[0], p1_move[1], "X")
+        print(grid)
+        input()
+        p2_move = find_best_move(grid, "O", "X")
+        if not p2_move == (-1, -1):
+            grid.add_piece(p2_move[0], p2_move[1], "O")
+        print(grid)
+        input()
+
+
+def measure_times():
+    grids = []
+    for i in range(100):
+        grid = Grid()
+        for column in grid.array:
+            for piece in column:
+                piece.set_symbol(random.choice(("X", "O", None)))
+
+        grids.append(grid)
+    start = time.time()
+    for grid in grids:
+        find_best_move(grid)
+
+    return time.time() - start
+
+
+def measure_times_alpha_beta():
+    grids = []
+    for i in range(100):
+        grid = Grid()
+        for column in grid.array:
+            for piece in column:
+                piece.set_symbol(random.choice(("X", "O", None)))
+
+        grids.append(grid)
+
+    start = time.time()
+    for grid in grids:
+        find_best_move_alpha_beta(grid)
+
+    return time.time() - start
 
 
 if __name__ == "__main__":
     x = Grid()
-    print(x)
-    print(find_best_move(x))
+    print(measure_times())
+    print(measure_times_alpha_beta())
