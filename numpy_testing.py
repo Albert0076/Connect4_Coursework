1ad0582a723b15386e21fd3b2507699c9baeeb55
Albert0076 <albain0809@highgateschool.org.uk>
2024-10-04 09:36:12 +0100

    added a convolution method for detecting wins

--- numpy_testing.py
+++ numpy_testing.py
@@ -1,11 +1,13 @@
 import numpy as np
 from connect4_structure_prototype import Grid, Cell
-import random
+from scipy import signal
 
 MAX = 10
 MIN = -10
 
-pos_weights = [0, 1, 2, 3, 3, 2, 1 ,0]
+pos_weights = [0, 1, 2, 3, 3, 2, 1, 0]
+
+
 # Player 1: 1, Player 2: 2, Empty: 0
 
 
@@ -22,116 +24,51 @@ def convert_to_np(grid: Grid):
     return np_grid
 
 
-def check_win(grid, p_num: int):
-    # This function is based of one found on StackOverflow by user 101arrowz Link:
-    # https://stackoverflow.com/questions/56351369/easier-way-to-check-for-four-in-row-column-diagonal-in-connect
-    # -four-game
-    win_num: int = 0
-    found = False
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            if grid[i, j] == 0:
-                break
-                # If the connect-4 game is working if there is an empty cell there cannot be any full cells above it,
-                # So we can go to the next column
+def get_kernels(size):
+    kernel_h = np.array([[1 for i in range(size)]])
+    kernel_v = np.array([[1] for i in range(size)])
+    kernel_d1 = np.array([[1 if i == j else 0 for j in range(size)] for i in range(size)])
+    kernel_d2 = np.array([[1 if size - i == j + 1 else 0 for j in range(size)] for i in range(size)])
+    return kernel_h, kernel_v, kernel_d1, kernel_d2
 
-            try:  # Vertical
-                if grid[i, j] == grid[i + 1, j] == grid[i + 2, j] == grid[i + 3, j]:
-                    # The computer is intended for default rules for now so we only have to worry about checking for 4.
-                    found = True
-                    win_num = grid[i, j]
 
-            except IndexError:
-                pass
+kernel_3 = get_kernels(3)
+kernel_4 = get_kernels(4)
 
-            try:  # Horizontal
-                if grid[i, j] == grid[i, j + 1] == grid[i, j + 2] == grid[i, j + 3]:
-                    found = True
-                    win_num = grid[i, j]
 
-            except IndexError:
-                pass
+def check_win(grid, p_num, op_num):
+    p_num_grid = np.copy(grid)
+    op_num_grid = np.copy(grid)
+    p_num_grid[p_num_grid != p_num] = 0
+    p_num_grid[p_num_grid == p_num] = 1
+    op_num_grid[op_num_grid != op_num] = 0
+    op_num_grid[op_num_grid == op_num] = 1
+    if any([4 in signal.fftconvolve(p_num_grid, kernel) for kernel in kernel_4]):
+        return MAX
 
-            try:  # Diagonal 1
-                if grid[i, j] == grid[i + 1, j + 1] == grid[i + 2, j + 2] == grid[i + 3, j + 3]:
-                    found = True
-                    win_num = grid[i, j]
-
-            except IndexError:
-                pass
-
-            try:  # Diagonal 2
-                if grid[i, j] == grid[i + 1, j - 1] == grid[i + 2, j - 2] == grid[i + 3, j - 3]:
-                    found = True
-                    win_num = grid[i, j]
-
-            except IndexError:
-                pass
-
-            if found:
-                if win_num == p_num:
-                    return MAX
-
-                else:
-                    return MIN
+    if any([4 in signal.fftconvolve(op_num_grid, kernel) for kernel in kernel_4]):
+        return MIN
 
     return 0
 
 
 def evaluate(grid, p_num, op_num):
+    p_num_grid = np.copy(grid)
+    op_num_grid = np.copy(grid)
+    p_num_grid[p_num_grid != p_num] = 0
+    p_num_grid[p_num_grid == p_num] = 1
+    op_num_grid[op_num_grid != op_num] = 0
+    op_num_grid[op_num_grid == op_num] = 1
     three_count = 0
-    pos_count = 0
-    piece_count = 0
-    # use the same code as check win just only checking for three in a rows.
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            if grid[i, j] == p_num:
-                piece_count += 1
-                win_change = 1
-                pos_count += pos_weights[i]
+    for kernel in kernel_3:
+        three_count += np.sum(signal.fftconvolve(p_num_grid, kernel) == 3)
+        three_count -= np.sum(signal.fftconvolve(op_num_grid, kernel) == 3)
 
-            elif grid[i, j] == op_num:
-                piece_count += 1
-                win_change = -1
-                pos_count -= pos_weights[i]
-
-            else:
-                win_change = 0
-            if grid[i, j] == 0:
-                break
-            try:  # Vertical
-                if grid[i, j] == grid[i + 1, j] == grid[i + 2, j]:
-                    three_count += win_change
-
-            except IndexError:
-                pass
-
-            try:
-                if grid[i, j] == grid[i, j + 1] == grid[i, j + 2]:
-                    three_count += win_change
-
-            except IndexError:
-                pass
-
-            try:
-                if grid[i, j] == grid[i + 1, j + 1] == grid[i + 2, j + 2]:
-                    three_count += win_change
-            except IndexError:
-                pass
-
-            try:
-                if grid[i, j] == grid[i + 1, j - 1] == grid[i + 2, j - 2]:
-                    three_count += win_change
-
-            except IndexError:
-                pass
-
-    return_val = 2*three_count + pos_count//piece_count
+    return_val = 2 * three_count
     if return_val >= MAX:
-        return_val = MAX-1
+        return_val = MAX - 1
     if return_val <= MIN:
-        return_val = MIN+1
-
+        return_val = MIN + 1
 
     return return_val
 
@@ -141,7 +78,7 @@ def minimax(is_max, grid, alpha, beta, depth, p_num):
         op_num = 2
     else:
         op_num = 1
-    win = check_win(grid, p_num)
+    win = check_win(grid, p_num, op_num)
     if win != 0:
         return win
 
@@ -181,18 +118,13 @@ def minimax(is_max, grid, alpha, beta, depth, p_num):
 
 
 def find_best_move(grid, p_num):
-    if p_num == 1:
-        player_dict = {True: 1, False: 2}
-    else:
-        player_dict = {True: 2, False: 1}
-
     alpha = MIN
     beta = MAX
     best = MIN
     possible_moves = []
     for i in range(grid.shape[0]):
         try:
-            val = minimax(False, add_piece(grid, p_num, i), alpha, beta, 8, p_num)
+            val = minimax(False, add_piece(grid, p_num, i), alpha, beta, 6, p_num)
             best = max(best, val)
             alpha = max(best, alpha)
             possible_moves.append(val)
@@ -223,14 +155,6 @@ def is_full(grid):
 
 
 if __name__ == "__main__":
-    grid = convert_to_np(Grid())
-    for i in range(3):
-        grid = add_piece(grid, 1, 5)
-    print(find_best_move(grid, 1))
-
-
-
-
-
+    pass
 
 # May need a more sophisticated evaluate function in order to make it make better and faster moves
