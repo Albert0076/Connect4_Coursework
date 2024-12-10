5aa5beff8558595a82d2b9d67ac960ebb3824875
Albert0076 <albain0809@highgateschool.org.uk>
2024-12-10 13:01:55 +0000

    Added many docstrings and comments to back_end.py and connect4_structure_prototype.py. Made some minor code changed that have not been properly tested.

--- back_end.py
+++ back_end.py
@@ -5,46 +5,99 @@ from binary_string_minimax_test import find_best_move, get_bit_mask
 
 
 class Game:
-    def __init__(self, cli, num_rows=6, num_columns=7, win_num=4):
-        self.interface = cli
+    def __init__(self, interface, num_rows=6, num_columns=7, win_num=4):
+        """
+        Parameters
+        ----------
+        interface: CLI
+            The interface being used for the game.
+        num_rows: int
+            The number of rows in the grid.
+        num_columns: int
+            The number of columns in the grid.
+        win_num: int
+            The number of symbols in a row needed to win.
+
+        """
+        self.interface = interface
         self.num_rows = num_rows
         self.num_columns = num_columns
+
         self.grid = Grid(num_rows, num_columns, win_num)
-        self.symbols = ['R', 'B', 'G', 'X']
-        self.players = []
-        self.game_over = False
-        self.current_player_num: int = 0
-        self.turn_num = 0
-        self.past_states = {}
+
+        self.symbols: list = ['R', 'B', 'G', 'X'] # The default symbols to use if invalid symbols are given by the Interface
+
+        self.players: list = [] # The players in the game
+        self.game_over: bool = False
+        self.current_player_num: int = 0 # The index of the player whose turn it is
+        self.current_player = None
+        self.turn_num: int = 0
+        self.past_states: dict = {} # A dictionary of past game states with the turn num as the index
 
     def add_human_player(self, name: str, symbol=""):
+        """
+        Adds a human player to the list of players.
+        Parameters
+        ----------
+        name: str
+            The name of the player.
+        symbol: str
+            The symbol of the player.
+
+        """
         self.players.append(Player(self, name, symbol))
 
     def add_computer_player(self, name: str, difficulty: int, symbol=""):
+        """
+        Adds a computer player to the list of players.
+        Parameters
+        ----------
+        name: str
+            The name of the player.
+        difficulty: int
+            The difficulty of the player.
+        symbol: str
+            The symbol of the player.
+
+        """
         self.players.append(ComputerPlayer(self, name, difficulty, symbol))
 
-    def play_game(self):
-        self.interface.display_grid()
-        self.add_to_past_dict(None)
-        while not self.game_over:
-            current_player = self.players[self.current_player_num]
-            move_made = False
-            move = None
-            while not move_made:
-                try:
-                    move = current_player.get_move()
-                    self.grid.add_piece(move, current_player.symbol)
-                    move_made = True
-                    self.add_to_past_dict(move)
 
-                except IndexError as error:
-                    current_player.register_error(error)
+    def make_player_move(self):
+        """
+        Gets a valid move from the player and then makes that move in the grid.
+
+        """
+        move_made = False
+        while not move_made:
+            try:
+                # If an index error is raised that means that it is an invalid move (likely because the column is full)
+                # so we ask the player to make it again
+                move = self.current_player.get_move()
+                self.grid.add_piece(move, self.current_player.symbol)
+                move_made = True
+                self.add_to_past_dict(move)
+
+            except IndexError as error:
+                self.current_player.register_error(error)
+
+
+    def play_game(self):
+        """
+        Main logic of the game. Loops through the players until the game has ended
+
+        """
+        self.current_player = self.players[self.current_player_num]
+        self.interface.display_grid()
+        while not self.game_over:
+            self.make_player_move()
 
             self.interface.display_grid()
             self.turn_num += 1
+
             if self.grid.check_win():
                 self.game_over = True
-                self.interface.display_win(current_player)
+                self.interface.display_win(self.current_player)
 
             elif self.grid.grid_full():
                 self.game_over = True
@@ -54,6 +107,14 @@ class Game:
                 self.current_player_num = (self.current_player_num + 1) % len(self.players)
 
     def add_to_past_dict(self, move_made):
+        """
+        Adds the current grid and the move to the past dictionary.
+        Parameters
+        ----------
+        move_made: int
+            The move that the current player has just made.
+
+        """
         if move_made is None:
             self.past_states[self.turn_num] = copy.deepcopy(self.grid), None, None
 
@@ -68,6 +129,16 @@ class Game:
 
 class Player:
     def __init__(self, game: Game, name: str, symbol=""):
+        """
+        Parameters
+        ----------
+        game: Game
+            The game object.
+        name: str
+            The name of the player.
+        symbol: str
+            The symbol of the player.
+        """
         self.game = game
         self.name = name
         if symbol == "":
@@ -77,9 +148,24 @@ class Player:
             self.symbol = symbol
 
     def get_move(self):
+        """
+        Gets the move from the player through the interface
+        Returns
+        -------
+        int
+            The move the player has made.
+
+        """
         return self.game.interface.get_move(self)
 
     def register_error(self, error):
+        """
+        Notifies the interface that an error has occurred.
+        Parameters
+        ----------
+        error
+
+        """
         self.game.interface.display_invalid_move(error)
 
 
--- connect4_structure_prototype.py
+++ connect4_structure_prototype.py
@@ -1,39 +1,76 @@
 class Grid:
     def __init__(self, num_rows=6, num_columns=7, win_num=4):
+        """
+        Parameters
+        ----------
+        num_rows: int
+            The number of rows in the grid.
+        num_columns: int
+            The number of columns in the grid.
+        win_num: int
+            The number of symbols in a row needed to win.
+        """
         self.win_num = win_num
         self.num_rows = num_rows
         self.num_columns = num_columns
         self.winning_symbol = None
 
-        self.cells = {(i, j): Cell(i, j) for i in range(self.num_rows) for j in range(self.num_columns)}
+        # Cells ares stored in a dictionary, with a tuple of coordinates as the key and the Cell class as the data
+        self.cells = {(row, column): Cell(row, column) for row in range(self.num_rows) for column in range(self.num_columns)}
 
-        self.columns = [[(i, j) for i in range(self.num_rows)] for j in range(self.num_columns)]
+        # The rows, columns and diagonals are then stored as lists of tuples which can then be checked in the dictionary
+        self.columns = [[(row, column) for row in range(self.num_rows)] for column in range(self.num_columns)]
 
-        self.rows = [[(i, j) for j in range(self.num_columns)] for i in range(self.num_rows)]
+        self.rows = [[(row, column) for column in range(self.num_columns)] for row in range(self.num_rows)]
 
-        self.SW_diagonals = [[(i, j) for i in range(self.num_rows) for j in range(self.num_columns) if i + j == k]
+        self.SW_diagonals = [[(row, column) for row in range(self.num_rows) for column in range(self.num_columns) if row + column == k]
                              for k in range(2 * min(self.num_columns, self.num_rows))]
 
         self.SE_diagonals = [
-            [(i, self.num_columns - j - 1) for i in range(self.num_rows) for j in range(self.num_columns) if i + j == k]
+            [(row, self.num_columns - column - 1) for row in range(self.num_rows) for column in range(self.num_columns) if row + column == k]
             for k in range(2 * min(self.num_columns, self.num_rows))]
 
         self.all_lines = self.columns + self.rows + self.SW_diagonals + self.SE_diagonals
 
     def column_height(self, column: int):
         """
-        :param column: column number
-        :return: the number of full cells in the column
-        :raise:
-            ValueError if column is not an integer or is not in the grid
+        Returns the number of pieces in a column.
+
+        Parameters
+        ----------
+        column: int
+            The column in the grid.
+
+        Returns
+        -------
+        int
+            The height of the column.
+
         """
         if not (isinstance(column, int) and -1 <= column <= self.num_columns):
             raise ValueError
         return sum([1 if not self.cells[cell].is_empty() else 0 for cell in self.columns[column]])
 
     def add_piece(self, column: int, symbol: str):
+        """
+        Adds a piece to a column in the grid.
+
+        Parameters
+        ----------
+        column: int
+            The column the piece is being added too.
+        symbol: str
+            The symbol of the piece being added.
+
+        Raises
+        ------
+        IndexError
+            If the column is full or not on the grid.
+
+        """
         added = False
         count = 0
+        # Loop up the column until a free cell is encountered and then stop looping up
         while not added and count < self.num_columns:
             if self.cells[self.columns[column][count]].is_empty():
                 added = True
@@ -43,41 +80,103 @@ class Grid:
                 count += 1
 
         if not added:
+            # If it loops through the column without finding an empty cell it will raise an IndexError
             raise IndexError("Column is full.")
 
     def check_win(self):
+        """
+        Checks to see if the grid has an n in a row.
+        Returns
+        -------
+        bool
+            whether the grid has an n in a row.
+
+        """
+        # Check line on all the lines and then see if any of them have a win
         return any([self.check_line(line) for line in self.all_lines])
 
     def check_line(self, line: list):
+        """
+        Checks to see if a line has an n in a row.
+        Parameters
+        ----------
+        line: list
+            The line to check.
+
+        Returns
+        -------
+        bool
+            whether the line has an n in a row.
+
+        """
         if len(line) < self.win_num:
+            # If the length of the line is less than the win length we know it cannot have a win
             return False
 
         else:
-            for i in range(len(line) - self.win_num + 1):
-                won = True
-                for j in range(self.win_num - 1):
-                    if self.cells[line[i + j]] != self.cells[line[i + j + 1]]:
-                        won = False
+            for start_index in range(len(line) - self.win_num + 1):
+                # We loop through from the first position in the line, up to the last one where a win could still be possible
+                won = all([self.cells[line[start_index]] == self.cells[line[start_index + relative_index]] for relative_index in range(self.win_num)])
 
                 if won:
-                    self.winning_symbol = self.cells[line[i]].symbol
+                    # If a line has n in a row we find the winning symbol and then return True
+                    # One potential issue with this system is that we cannot detect if there are two different wins from
+                    # different players however the logic of the game should prevent this from happening
+                    self.winning_symbol = self.cells[line[start_index]].symbol
                     return True
 
         return False
 
     def line_full(self, line):
-        return all([self.cells[index].is_empty() for index in line])
+        """
+        Checks to see if a line is full.
+        Parameters
+        ----------
+        line: list
+            The line to check.
+
+        Returns
+        -------
+        bool
+            Whether the line is full.
+
+        """
+        return all([self.cells[position].is_empty() for position in line])
 
     def grid_full(self):
+        """
+        Checks to see if the grid is full.
+        Returns
+        -------
+        bool
+            Whether the grid is full.
+
+        """
         return all([self.line_full(line) for line in self.all_lines])
 
     def __repr__(self):
+        """
+        __repr__ function for grid.
+        Returns
+        -------
+        str
+            Grid(num_rows, num_columns, win_num)
+
+        """
         return f"Grid({self.num_rows=}, {self.num_columns=}, {self.win_num=})"
 
     def __str__(self):
+        """
+        __str__ function which shows the grid in an understandable way.
+        Returns
+        -------
+        str
+            All the cells including end lines
+
+        """
         return_str = ""
-        for i in range(len(self.rows) - 1, -1, -1):
-            for cell in self.rows[i]:
+        for row in range(len(self.rows) - 1, -1, -1):
+            for cell in self.rows[row]:
                 if self.cells[cell].is_empty():
                     return_str += "|_| "
 
@@ -86,29 +185,75 @@ class Grid:
 
             return_str += "\n"
 
-        for i in range(len(self.columns)):
-            return_str += f" {i + 1}  "
+        for row in range(len(self.columns)):
+            return_str += f" {row + 1}  "
 
         return return_str
 
 
 class Cell:
-    def __init__(self, i: int, j: int):
-        self.row = i
-        self.column = j
+    def __init__(self, row: int, column: int):
+        """
+        Parameters
+        ----------
+        row: int
+            The row of the cell.
+        column: int
+            The column of the cell.
+
+        """
+        self.row = row
+        self.column = column
         self.symbol = None
 
     def is_empty(self):
+        """
+        Checks if the cell is empty.
+        Returns
+        -------
+        bool
+            Whether the symbol of the cell is none.
+
+        """
         return self.symbol is None
 
     def set_symbol(self, symbol: str):
+        """
+        Sets the symbol of the cell.
+        Parameters
+        ----------
+        symbol: str
+            The symbol of the cell.
+
+        """
         self.symbol = symbol
 
     def __eq__(self, other):
+        """
+        Checks to see if too cells have the same symbol.
+        Parameters
+        ----------
+        other: Cell
+            The other cell to check.
+
+        Returns
+        -------
+        bool
+            Whether the symbols of the cells are equal and not None.
+
+        """
         return self.symbol == other.symbol and not self.symbol is None
 
     def __repr__(self):
-        return f"Cell({self.row}, {self.column}, {self.symbol})"
+        """
+        __repr__ function for cell.
+        Returns
+        -------
+        str
+            Cell(row=, column=, symbol=)
+
+        """
+        return f"Cell({self.row=}, {self.column=}, {self.symbol=})"
 
 
 if __name__ == "__main__":
